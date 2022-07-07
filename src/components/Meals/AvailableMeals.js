bed56258b44f7773c8a5039127ce913305de1b6d
Ryan Cabanas <ryangithub777@gmail.com>
2022-07-07 19:03:01 -0400

    - Add a `Card` component to wrap the individual meals so that they present on a   nice white background.
    - Add a `MealItem` component that will handle the layout and styling of the
      individual meals that sit in the `Card` component, which sits in the
      `AvailableMeals` component.  Note that we use this new `MealItem` component
      to replace the temporary `<li>` we were using before for the meal items when
      putting together the list of meals in an array.
    
    - We will be working on the `MealItemForm` component next, which will be used
      inside of the `MealItem` component.

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,15 +1,26 @@
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
 import { DUMMY_MEALS } from './dummy-meals';
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => {
-    return <li>{meal.name}</li>;
+    return (
+      <MealItem
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    );
   });
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
