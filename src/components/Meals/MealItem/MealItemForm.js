21320b29b2546ae6e4090a0a5fdd93fe6c212ea9
Ryan Cabanas <ryangithub777@gmail.com>
2022-07-09 14:36:07 -0400

    End of Lesson 145: Adding items to the cart using context and forwardRef.
    - Starting in the `MealItemForm` component, add `useState` to track the amount
      of items to be added to the card.  In order to get the value from the `input`,
      we need to dive into the `Input` component (which holds the `input` element)
      and get to that value using the `forwardRef` approach.  Therefore, we create
      a `ref` in the `MealItemForm` component (using the `useRef` hook) and pass it
      down throuth the `Input` component to the `input` element,.
    
      On form submission, we get this `input` value (using the `ref`), we do some
      validation, and we add the amount (count, really) back up to the `MealItem`
      component via the prop function `onAddToCart`.
    
      Additionally, if the amount/count isn't valid, we output an error message.
      (But really, this message isn't being used because of the `min` and `max`
      properties we added on the `input` element.)
    
    - In the `MealItem` component, we add the global "cart" context and set that up.
      Then we create the `addItemToCartHandler` function, that we pass down to
      `MealItemForm` via the `onAddToCart` prop, and we use the context there to
      call the `addItem` function to pass our item to the global context, which will
      get our item into the cart.

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,35 @@
+import { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value; // "value" will always be a string
+    const enteredAmountNumber = +enteredAmount; // The "+" in front converts this string to a number
+
+    if (
+      enteredAmount.trim() === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    setAmountIsValid(true);
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: `amount_${props.id}`,
@@ -15,7 +40,8 @@ const MealItemForm = (props) => {
           defaultValue: '1',
         }}
       />
-      <button>+ Add</button>
+      <button type="submit">+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
