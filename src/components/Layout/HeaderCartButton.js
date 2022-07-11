3d4040b28bd00b216cac0bc19db559e11c0bdf81
Ryan Cabanas <ryangithub777@gmail.com>
2022-07-10 23:33:04 -0400

    End of Lesson 149: Adding an animation to the header cart button.
    - This is really pretty slick.  Whenever we add a meal item to the cart, the
      header cart button play a "bump" animation.  How this is done is pretty slick.
    
    - First we need to import `useEffect` and `useState` hooks.
    
    - We create a state to track when the cart has been changed (ie - when a meal
      has been added to the cart).
    
    - In the CSS, there was an additional class already added named `bump`.  Now,
      the button already has a class named `button` added to it, but when we also
      add the `bump` CSS class to the button, this will cause the button to animate
      when the page is rendered ***and*** the `bump` CSS class is present.
    
    - To conditionally add (and remove) the `bump` CSS class from the button, we
      create a `btnClasses` variable that will handle adding the `bump` CSS class,
      or not.  We use this `btnClasses` CSS class on the button.
    
      Now, as you can see, the `bump` CSS class is conditionally added depending on
      the state of `cartIsChanged`.  Therefore, this is the state we need to control
      to add/remove the `bump` CSS class.  This is where the `useEffect` comes in.
    
    - Remember that after initial page render, the `useEffect` will always run at
      least once.  Additonally, we add a dependency to `useEffect` so that whenever
      the list of items changes in any way, we run the `useEffect` again.
    
      On the first run of `useEffect` (after the initial page render), since the
      cart will be empty and the `items` array length will be 0, we just `return` so
      that nothing happens regarding the `useEffect`.
    
      If the `useEffect` fires, though, because the `items` array has changed, then
      the rest of the `useEffect` runs, which is to set the `cartIsChanged` state to
      `true`.  Because of the state change, the page re-renders.  When it re-renders,
      the `btnClasses` variable will now change so that it includes the `bump` CSS
      class, which will now cause the header cart button to "bump" (because it gets
      rendered with the `bump` CSS class in place).
    
    - In the `useEffect`, we also set a timer to set `cartIsChanged` back to false
      after 300ms (because that's how long the animation runs for).  So after that
      time, `cartIsChanged` is set back to false.  Now here's the cool part.  Yes,
      because the state changes again, the page will re-render once again, ***but***
      because `cartIsChanged` is false, the `bump` CSS class will be removed and the
      header cart button ***doesn't*** "bump" again this time because the `bump` CSS
      class is no longer there.  Cool!  Now we're all reset and ready for another
      cart change and another "bump"!
    
    - Also note/remember that we need to `return` a cleanup function at the end of
      the `useEffect` whenever we use a timer, or some other sort of subscription,
      to avoid memory leaks.  In our case, multiple timers ***could*** be started
      if we kept adding items to the cart quickly (quicker than 300ms).  Adding this
      cleanup function (with the `clearTimeout` function inside of it) will ensure
      that the current timer is killed before invoking another one.

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,38 @@
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
-  const cartCtx = useContext(CartContext);
+  const [cartIsChanged, setCartIsChanged] = useState(false);
 
-  const numberOfCartItems = cartCtx.items.reduce(
+  const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce(
     (prevNumber, item) => prevNumber + item.amount,
     0
   );
 
+  const btnClasses = `${classes.button} ${cartIsChanged ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setCartIsChanged(true);
+
+    const timer = setTimeout(() => {
+      setCartIsChanged(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onShowCart}>
+    <button className={btnClasses} onClick={props.onShowCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
