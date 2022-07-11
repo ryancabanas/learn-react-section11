27b31c5991214230340b324641777260d02dd0e9
Ryan Cabanas <ryangithub777@gmail.com>
2022-07-10 21:56:30 -0400

    End of Lesson 147: Add more of an existing item to the cart.
    - Previously, we had a simplistic logic that ***always*** added items to the
      cart as separate objects, even if that item already existed in the cart.
    
      Update the logic so that we check to see if an item already exists in the
      cart.  If so, just update the amount.

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,9 +8,21 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    let updatedItems = [];
+    if (existingCartItemIndex === -1) {
+      updatedItems = state.items.concat(action.item);
+    } else {
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex].amount += action.item.amount;
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
